d4417849674ff8ede4dca567085ea08717f0d4b1
PandaLue <sdkane@mtu.edu>
2023-08-18 11:32:53 +0200

    initial working commit

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from PIL import Image as PILImage
 from io import BytesIO
 from tqdm import tqdm
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate, Image
-from reportlab.lib import colors
-from fpdf import FPDF
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
 
 """
 Imports Cards from a txt file (preferably from mtggoldfish), then gathers the images from scryfall.
 Stores the images in an A4 for printing... somehow
+TODO: Print multiple basics, or any amount per .
 """
 
 file_path = 'Deck - Rock the Dinosore.txt' # input path
@@ -24,58 +24,61 @@ print(card_list[columns])
 
 cards = []
 image_urls = []
+card_counts = []
 
 # Initial Scryfall query to get the image url data
 for card in tqdm(card_list[columns].values):
-    cards.append(scrython.Named(exact=card[0][2:])) # trims the numbers from MTG gold fish, shouldn't be a problem with csvs
+    cards.append(scrython.Named(exact=card[0][2:])) # trims the numbers from MTG gold fish, shouldn't be a problem with csvs, breaks with more than 1 integer
+    card_counts.append*card[0][0:2]
     time.sleep(0.1)
     image_urls.append(cards[-1].image_uris()['normal'])
 
 # Second scryfall query to get the images
 images = []
-image_width = A4[0] / 3 - 10
-image_height =A4[1] / 3 - 10
+image_width = int(A4[0] / 3) - 10
+image_height = int(A4[1] / 3) - 10
 for url in tqdm(image_urls):
     time.sleep(0.1)
     response = requests.get(url)
     if response.status_code == 200:
-        image_contnt = BytesIO(response.content)
+        image_contnt = PILImage.open(BytesIO(response.content))
+        image_contnt = image_contnt.resize((image_width, image_height))
         # img = PILImage.open(BytesIO(image_contnt))
-        image = Image(image_contnt, width=image_width, height=image_height)
-        images.append(image)
+        # image = Image(image_contnt, width=image_width, height=image_height)
+        images.append(image_contnt)
     else:
         print('Failed to grab image')
 
-# A list of rows for the images
-doc = SimpleDocTemplate(output_path, pagesize=A4)
-
-image_rows = []
-for i in range(0, len(image_urls), 3):
-    image_row = images[i:i+3]
-    image_rows.append(image_row)
-    doc.build(image_row)
-
-# create of list of rows for each page
-rows_per_page = 3
-page_numbers = int(len(image_rows) / rows_per_page) + 1
-# for i in range(page_numbers):
-
-# rows_to_render = image_rows[:rows_per_page]
-
 # render the pdf
+c = canvas.Canvas(output_path, pagesize=A4)
 
+cell_width = 2.25 * inch
+cell_height = 3.25 * inch
+x_start = 0.5 * inch
+y_start = A4[1] - 0.5 * inch - cell_height  # Start from the top of the page
 
-# pdf = FPDF()
-# pdf.set_auto_page_break(auto=True, margin=15)
-# pdf.add_page()
-# for i, row in tqdm(enumerate(image_rows)):
-#     for j, url in enumerate(row):
-#         time.sleep(0.1)
-#         position = url.find('jpg')
-#         lookup = url[:position+len('jpg')]
-#         pdf.image(name=lookup, x=i % 3, y=j, w=image_width, h=image_height, type='JPG')
+# Loop through image paths and add images to the grid
 
+row_number = int(len(images) / 3) +1
+
+for row in range(row_number):
+    for col in range(3):
+        image_index = row * 3 + col
+        if image_index < len(images):
+            image_path = images[image_index]
+            c.drawInlineImage(image_path, x_start + col * cell_width, y_start - (row % 3 ) * cell_height, width=cell_width, height=cell_height)
+
+            # Draw a border around the image cell
+            c.rect(x_start + col * cell_width, y_start - (row % 3) * cell_height, cell_width, cell_height)
     
+    if row % 3 == 0:
+        c.showPage()
+        x_start = 0.5 * inch
+        y_start = A4[1] - 0.5 * inch - cell_height
 
-# save the PDF
-# pdf.output(output_path)
+
+
+# Save the PDF
+c.save()
+
+print("PDF with image grid on A4 created successfully.")
