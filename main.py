c0f98987a5e73d9b4c7ede80d9554c9efe53d91c
PandaLue <sdkane@mtu.edu>
2023-10-02 23:49:39 +0200

    back to a working version

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ for card in tqdm(card_list[columns].values):
     # image_urls.append(cards[-1].card.image_uris()['large'])
 
 # Second scryfall query to get the images
-images = []
 image_width = int(A4[0] / 3) - 10
 image_height = int(A4[1] / 3) - 10
 for card in tqdm(cards):
@@ -61,9 +60,7 @@ for card in tqdm(cards):
         image_contnt = PILImage.open(BytesIO(response.content))
         image_contnt = image_contnt.resize((image_width, image_height))
         card.image = image_contnt
-        # img = PILImage.open(BytesIO(image_contnt))
-        # image = Image(image_contnt, width=image_width, height=image_height)
-        # images.append(image_contnt)
+
     else:
         print('Failed to grab image')
 
@@ -80,13 +77,15 @@ y_start = A4[1] - 0.5 * inch - cell_height  # Start from the top of the page
 card_sum = sum([i.card_count for i in cards])
 # render_images = [card.image * card.card_count for card in cards]
 render_images = [card.image for i in range(card.card_count) for card in cards]
+render_count = [card.card_count for card in cards]
 row_number = int(card_sum / 3) +1
 
-for row in tqdm(range(row_number)):
+for row in range(row_number):
     for col in range(3):
         image_index = row * 3 + col
-        if image_index < len(images):
+        if image_index < len(render_images):
             image_path = render_images[image_index]
+
             c.drawInlineImage(image_path, x_start + col * cell_width, y_start - (row % 3 ) * cell_height, 
                               width=cell_width, height=cell_height)
 
